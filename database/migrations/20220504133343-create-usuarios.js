197c954627a0d31b93656e2ef755ec3db6794ea1
Henrique Pereira <henriquepereiradasilva2013@gmail.com>
2022-05-09 09:18:49 -0300

    configurando migrations

---
+++ database/migrations/20220504133343-create-usuarios.js
@@ -0,0 +1,43 @@
+'use strict';
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    // Adicionando alterações ao migrate
+
+    await queryInterface.createTable(
+      "usuarios", {
+        id: {
+          type: Sequelize.DataTypes.INTEGER,
+          primaryKey: true,
+          autoIncrement: true,
+          allowNull: false
+        },
+        nome: {
+          type: Sequelize.DataTypes.STRING(45),
+          allowNull: false
+        },
+        email: {
+          type: Sequelize.DataTypes.STRING(45),
+          allowNull: false
+        },
+        senha: {
+          type: Sequelize.DataTypes.STRING(64),
+          allowNull: false,
+        },
+        foto: {
+          type: Sequelize.DataTypes.STRING(45),
+          allowNull: true
+        },
+        createdAt: Sequelize.DataTypes.DATE,
+        updatedAt: Sequelize.DataTypes.DATE,
+        deletedAt: Sequelize.DataTypes.DATE
+      }
+    );
+  },
+
+  async down(queryInterface, Sequelize) {
+    
+    // revertendo migrate
+    await queryInterface.dropTable('usuarios');
+  }
+};
