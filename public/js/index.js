a969e15739a76b407006a900e6a8046f4fcc0b90
Henrique Pereira <henriquepereiradasilva2013@gmail.com>
2022-06-20 11:02:54 -0300

    Registrando usuario com foto e entrando na tela interna

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,5 @@
 // Verificar se o email foi preenchdo
-const verificarCampoPreenchido = (evento)=> {
+const verificarCampoPreenchido = (evento) => {
 
     console.log(evento.target);
 
@@ -14,8 +14,8 @@ const verificarCampoPreenchido = (evento)=> {
 };
 
 const onFileChange = evento => {
-   let img = document.getElementById("output");
-   img.src = URL.createObjectURL(evento.target.files[0])
+    let img = document.getElementById("output");
+    img.src = URL.createObjectURL(evento.target.files[0])
 }
 
 // 1 - Capturar os elementos do html para o js
@@ -35,32 +35,67 @@ inputNome.addEventListener("blur", verificarCampoPreenchido);
 inputEmail.addEventListener("blur", verificarCampoPreenchido);
 inputSenha.addEventListener("blur", verificarCampoPreenchido);
 inputFile.addEventListener("change", onFileChange);
-    // Construindo um objeto
-    let corpoDaRequisicao = {
-        nome: inputNome.value,
-        email: inputEmail.value,
-        senha: inputSenha.value
-    } 
+// Construindo um objeto
+let corpoDaRequisicao = {
+    nome: inputNome.value,
+    email: inputEmail.value,
+    senha: inputSenha.value
+}
 
-form.addEventListener("submit", (evt)=>{
-    
+form.addEventListener("submit", async (evt) => {
+
     // impedir o formulario de enviar
     evt.preventDefault();
 
-// Levantando os dados do formulário
-let formData = new FormData(form);
+    // Levantando os dados do formulário
+    let formData = new FormData(form);
 
 
-    // fetch 
-    fetch("http://localhost:3000/api/v1/usuarios", 
-    {
+    // Enviando o formData para o servidor
+    // usando a função fetch (assíncrona)
+    let response = await fetch("http://localhost:3000/api/v1/usuarios", {
         method: "POST",
         body: formData,
         //headers:{}
+    });
+
+    if (response.status == 409) {
+        alert("Usuário já cadstrado")
     }
-    )
+    if (response.status == 500) {
+        alert("Erro. Tente novamente mais tarde.")
+    }
+    if (response.status == 201) {
+        let usuario = await response.json();
+        mostrarApp(usuario);
+    }
 
 });
+
+function mostrarApp(usuario){
+    console.log(usuario);
+
+    // Esconder a div de registro
+    document.getElementById("registro").style.display = "none";
+
+    // Mostrar a div da aplicação
+    document.getElementById("app").style.display = "block";
+
+    // Preencher os locais com as info do usuario
+
+    // Nome do usuario
+    document.getElementById("app-nome").innerText = usuario.nome;
+
+    // E-mail
+    let aEmail = document.getElementById("app-email");
+    aEmail.innerText = usuario.email;
+    aEmail.setAttribute("href", `mailto:${usuario.email}`);
+
+    // Imagem/Avatar
+    let imgAvatar = document.getElementById("app-avatar");
+    imgAvatar.setAttribute("alt", `Foto de ${usuario.nome}`);
+    imgAvatar.setAttribute("src", `img/avatares/${usuario.foto}`);
+}
 
 
 
